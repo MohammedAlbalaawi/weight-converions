9837ce2e50af08b7e2b99b1fd48e1311a23a85bf
Mohammed Albalaawi <m.cip@live.com>
2022-10-06 16:47:35 +0300

    test

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace MohammedAlbalaawi\WeightConversions;
 
+use MohammedAlbalaawi\WeightConversions\WeightConversionsClass;
+
 it('can convert kilograms to pounds correctly', function () {
     $pounds = WeightConversionsClass::kilograms(100)->toPounds();
     expect($pounds)->toEqual(220.46);
